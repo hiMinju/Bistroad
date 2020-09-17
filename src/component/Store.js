6d15aee8adf47ebe6b4907b28c2f99a32b668347
hiMinju <minju980508@gmail.com>
2020-09-18 02:00:17 +0900

    Adapt the menu items in each store page

--- src/component/Store.js
+++ src/component/Store.js
@@ -1,48 +1,49 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { hexToRgb } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import StoreList from './StoreList';
 import Api from '../Api';
 
 class Store extends Component {
-	// state = {
-	// 	information: [
-	// 		{
-	// 			id: 'id',
-	// 			storeId: 'storeId',
-	// 			name: 'name',
-	// 			description: 'description',
-	// 			price: 0,
-	// 			photoUri: null,
-	// 			starts: 0
-	// 		}
-	// 	]
-	// };
-	// static defaultProps = {
-	// 	info: []
-	// };
-	constructor(props) {
-		super(props);
-		this.state = { info: [], params: this.props.match.params };
-
-		this.setState({ storeId: this.state.params.storeId });
-		Api.get('/stores/' + this.state.params.storeId + '/items').then((resp) => {
-			console.log(this.state.params.storeId);
-			console.log(resp);
-			this.setState({ info: resp.data });
-			console.log(this.state.info);
-		});
-	}
+	state = {};
 
 	componentDidMount() {
-		this.setState({ storeId: this.state.params.storeId });
-		Api.get('/stores/' + this.state.params.storeId + '/items').then((resp) => {
-			console.log(this.state.params.storeId);
-			console.log(resp);
-			this.setState({ info: resp.data });
-			console.log(this.state.info);
-		});
+		this._getMenuList();
 	}
+
+	_renderMenu = () => {
+		const menu = this.state.menu.map((item, index) => {
+			console.log(item);
+			return (
+				<StoreList
+					id={item.id}
+					storeId={item.storeId}
+					name={item.name}
+					description={item.description}
+					price={item.price}
+					photoUri={item.photoUri}
+					stars={item.stars}
+					key={item.id}
+				/> //key prop으로 index 작성
+			);
+		});
+		return menu;
+	};
+
+	_getMenuList = async () => {
+		const menu = await this._callApi(); // _callApi 함수가 끝날 때까지 기다림
+		this.setState({
+			menu
+		});
+	};
+
+	_callApi = () => {
+		const { params } = this.props.match;
+
+		return Api.get('/stores/' + params.storeId + '/items')
+			.then((resp) => resp.data)
+			.catch((err) => console.log(err));
+	};
 
 	render() {
 		// const { id, storeId, name, storeName, description, price, photoUri, stars } = this.props.info;
@@ -50,22 +51,26 @@ class Store extends Component {
 		// Api.get('/stores/' + params.storeId + '/items').then(this.setState({ information: [ Response.data ] }));
 		const resp = Api.get('/stores');
 		console.log(resp);
+		const { menu } = this.state;
 		return (
 			<Typography
 				component="div"
 				align="center"
 				style={{
-					height: '100vh',
 					backgroundColor: hexToRgb('#FEDFDB'),
-					marginTop: '2rem'
+					padding: '50px',
+					display: 'flex',
+					justifyContent: 'space-around',
+					flexWrap: 'wrap',
+					fontSize: '14px'
 				}}
 			>
-				<h1 style={{ textAlign: 'left', display: 'inline', marginRight: '2rem' }} />
-				<h3 style={{ textAlign: 'left', display: 'inline' }} />
-				<p />
-				<div style={{ textAlign: 'left', marginTop: '5rem' }}>메뉴 리스트</div>
-				<div />
-				<StoreList data={this.state.info} />
+				<div style={{ textAlign: 'left', display: 'block', width: '100%' }}>메뉴 리스트</div>
+				<div>
+					{// 데이터가 없다면 'Loading'을 띄우고, 있으면 menu list가 보이도록 한다.
+					menu ? this._renderMenu() : 'Loading'}
+				</div>
+				{/* <StoreList data={this.state.info} /> */}
 			</Typography>
 		);
 	}
--- src/component/StoreList.js
+++ src/component/StoreList.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import Store from './Store';
+import PropTypes from 'prop-types';
+import '../styles/Store.css';
+import LinesEllipsis from 'react-lines-ellipsis';
 
-class StoreList extends Component {
-	static defaultProps = {
-		data: []
-	};
-	// URL에서 정보 얻기 가능?
-	render() {
-		const { data } = this.props;
-		const list = data.map((info) => <Store key={data.id} info={data} />);
-		console.log(list);
-		return <div>{list}</div>;
-	}
+function StoreList({ id, storeId, name, description, price, photoUri, stars }) {
+	return (
+		<div className="menu">
+			<div className="menu_column">
+				<MenuImage image={photoUri} />
+			</div>
+
+			<div className="menu_column">
+				<h1>{name}</h1>
+				<div className="menu_description">
+					<LinesEllipsis text={description} maxLine="3" ellipsis="..." trimRight basedOn="letters" />
+				</div>
+				<div className="menu_price">{price}</div>
+				<div className="menu_star">{stars}</div>
+			</div>
+		</div>
+	);
 }
+
+function MenuImage({ photoUri }) {
+	// return <img src={photoUri} alt="음식 이미지" className="foodImage" />;
+	return (
+		<img
+			src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fldb.phinf.naver.net%2F20200416_191%2F1587005465013afNYI_JPEG%2F%25BE%25F7%25C3%25BC%25C0%25DA%25C3%25BC_20200416_327581_%25B7%25B9%25B5%25E5175-%25B4%25EB%25C4%25A1%25BF%25AA%25C1%25A1_175%25B6%25B1%25BA%25BA%25C0%25CC_1080x640.jpg"
+			alt="음식 이미지"
+			className="menuImage"
+		/>
+	);
+}
+
+StoreList.propTypes = {
+	id: PropTypes.string.isRequired,
+	storeId: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	price: PropTypes.number.isRequired,
+	photoUri: PropTypes.string.isRequired,
+	stars: PropTypes.number.isRequired
+};
 
 export default StoreList;
